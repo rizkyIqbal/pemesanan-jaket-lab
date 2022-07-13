f24349f3f18dc6958c62bd8e3af65e3109be8592
IzzaFathony <izzafathony27@gmail.com>
2022-07-13 21:50:09 +0700

    Change int to string

--- database/migrations/2022_04_18_153135_create_transactions_table.php
+++ database/migrations/2022_04_18_153135_create_transactions_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->integer("user_id");
+            $table->string("user_id");
             $table->integer("jacket_id");
             $table->integer("size_id");
             $table->string("custom")->nullable();
