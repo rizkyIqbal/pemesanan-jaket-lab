a2dbc97946852e7d0ba0d646e92e1612b95d12c8
Izza Ihsan Fathony <izzafathony27@gmail.com>
2022-12-16 22:37:11 +0700

    seeder sizes and tracks

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +16,26 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $sizes = ["S", "M", "L", "XL", "2XL", "3XL"];
+        for($i = 0; $i < count($sizes); $i++) {
+            DB::table("sizes")->insert([
+                "name" => $sizes[$i],
+            ]);
+        }
+
+        $tracks = [
+            "Pemesanan Sudah Masuk",
+            "Pemesanan Sudah Masuk",
+            "Pembayaran Dikonfirmasi",
+            "Proses diPenjahit",
+            "Pesanan Jadi",
+            "Proses pengecekan",
+            "Siap diambil"
+        ];
+        for($i = 0; $i < count($tracks); $i++) {
+            DB::table("tracks")->insert([
+                "name" => $tracks[$i],
+            ]);
+        }
     }
 }
